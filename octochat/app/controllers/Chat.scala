1b8e804505c60bb941171ad8239f7bf9af576ba0
yoshik <fujiwara.access@gmail.com>
2013-02-02 20:40:10 +0900

    chat page

---
+++ octochat/app/controllers/Chat.scala
@@ -0,0 +1,12 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.libs.json._
+import play.api.mvc.WebSocket
+
+import models._
+
+object Chat extends Controller {
+
+}
--- octochat/app/controllers/Room.scala
+++ octochat/app/controllers/Room.scala
@@ -2,10 +2,17 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import controllers.Auth._
 
 object Room extends Controller {
   
-  def view(owner:String,repo:String) = TODO
-  
-  
+  def view(owner:String,repo:String) = Action {
+  implicit request =>
+  session.get("login").map {login=>
+      val user = GithubUser.fromSession(session)
+      Ok(views.html.room(user.login)(user.avatar_url))
+    }.getOrElse {
+      Ok(views.html.index("NotLogin"))
+    }
+  }
 }

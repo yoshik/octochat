f01019c67c0e0f9b8e9eab535bcb96802fc6f065
yoshik <fujiwara.access@gmail.com>
2013-02-06 22:02:59 +0900

    認証強化

--- octochat/app/controllers/Chat.scala
+++ octochat/app/controllers/Chat.scala
@@ -4,18 +4,19 @@ import play.api._
 import play.api.mvc._
 import play.api.libs.json._
 import play.api.mvc.WebSocket
-
-import models._
 import play.api.data._
 import play.api.data.Forms._
+import controllers.Auth._
+import models._
 
 object Chat extends Controller {
     
-  def post(owner:String,repo:String,author:String) = Action { implicit request =>
+  def post(owner:String,repo:String) = Action { implicit request =>
     messageForm.bindFromRequest.fold(
       errors => Redirect(routes.Room.view(owner,repo)),
       message => {
-        ChatModel.create(message,owner,repo,author)
+        val user = GithubUser.fromSession(session)
+        ChatModel.create(message,owner,repo,user.login)
         Redirect(routes.Room.view(owner,repo))
       }
     )
